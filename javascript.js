315071ed72c0c8ce4c6ce600c12da6e027d18929
Eduardo Pineda <edibozu@gmail.com>
2020-09-03 23:41:01 -0400

    added 2 more variables and added a condition which sets the min-max of characters

--- javascript.js
+++ javascript.js
@@ -1,95 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Possible character variables
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "-",
-  "_",
-  "=",
-  "+",
-  "[",
-  "]",
-  "{",
-  "}",
-  "<",
-  ">",
-  ",",
-  ".",
-  "/",
-  "?",
-  ":",
-  ":",
-  "~",
-  "`",
-];
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -97,11 +8,104 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
+// Possible character variables
+var possibleCharacters = "";
+var newPassword = "";
+var lowerCase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var upperCase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "-",
+    "_",
+    "=",
+    "+",
+    "[",
+    "]",
+    "{",
+    "}",
+    "<",
+    ">",
+    ",",
+    ".",
+    "/",
+    "?",
+    ":",
+    ":",
+    "~",
+    "`",
+  ];
+// Prompts and confirms to determine the level of security the user wants from the password
 function generatePassword() {
   var howManyCharacters = prompt(
     "How many characaters would you like the password to be, between 8-128"
   );
+  if(howManyCharacters < 8 || howManyCharacters > 128) {
+      prompt("ERROR! Numbeer has to be between 8 - 128");
+  }
 
   var lowerCaseCharacters = confirm(
     "Would you like to include lowercase characters?"
